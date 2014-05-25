8d11b3c7aa3ca9f9cab261fb29a41ae2d7ca3c37
sunita-sharma <sunitaas@yahoo.com>
2014-05-25 11:46:18 -0400

    Update BankTest.java

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -1,54 +1,70 @@
 package com.abc;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
-public class BankTest {
+public class BankTest extends TestCase {
     private static final double DOUBLE_DELTA = 1e-15;
 
     @Test
-    public void customerSummary() {
+    public void testCustomerSummary() {
         Bank bank = new Bank();
         Customer john = new Customer("John");
-        john.openAccount(new Account(Account.CHECKING));
+        john.openAccount(new Account(AccountTypeEnum.CHECKING));
         bank.addCustomer(john);
 
         assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
     }
 
     @Test
-    public void checkingAccount() {
+    public void testCheckingAccount() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.CHECKING);
+        Account checkingAccount = new Account(AccountTypeEnum.CHECKING);
         Customer bill = new Customer("Bill").openAccount(checkingAccount);
         bank.addCustomer(bill);
 
         checkingAccount.deposit(100.0);
 
-        assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
+        try {
+			assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
+			
+		} catch (ApplicationException e) {
+			 
+			e.printStackTrace(); //TODO add test case to test exception 
+		}
     }
 
     @Test
-    public void savings_account() {
+    public void testSavingsAccount() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.SAVINGS);
+        Account checkingAccount = new Account(AccountTypeEnum.SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
 
         checkingAccount.deposit(1500.0);
 
-        assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        try {
+			assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+		} catch (ApplicationException e) {
+			 
+			e.printStackTrace();
+		}
     }
 
     @Test
-    public void maxi_savings_account() {
+    public void testMaxiSavingsAccount() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        Account checkingAccount = new Account(AccountTypeEnum.MAXI_SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
 
         checkingAccount.deposit(3000.0);
 
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        try {
+			assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+		} catch (ApplicationException e) {
+			 
+			e.printStackTrace();
+		}
     }
 
 }
